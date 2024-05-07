422f96bb3c4904a7fe06ec7259394b8d73a86dea
Abhay kumar <abhayrobotics1908@gmail.com>
2024-05-07 19:13:29 +0530

    shimmer UI done , bulid pending

--- 004 Food App/src/components/ShimmerBody.js
+++ 004 Food App/src/components/ShimmerBody.js
@@ -1,28 +1,29 @@
-const ShimmerBody = ()=>{
-    let array=[]
-    for(let i=0;i<20;i++){
-
-     
-        array.push( <div className="shimmer__card flex  w-52 h-52 bg-slate-200 p-3 m-2 pointer border border-slate-300" key= {i} >
-                         <div className="shimmer__img"></div> 
-                         <div className="shimmer__line l1 "></div>
-                         <div className="shimmer__line l2"></div>
-                         <div className="shimmer__line l3"></div>
-                         <div className="shimmer"></div>
-                    </div>)
-     
-         i++;
-    }
-    // console.log(array)
-
- return(
-
-     <div className="shimmer__container flex flex-wrap">
-        {array}
+const ShimmerBody = () => {
+  let array = [];
+  for (let i = 0; i < 20; i++) {
+    array.push(
+      <div
+        className="shimmer__card flex  flex-col animate-pulse   m-3 pointer border  rounded-xl border-slate-300  w-[220px] h-[300px]  my-4  rounded-xl shadow-lg bg-slate-100  "
+        key={i}
+      >
+        {/* img */}
+        <div className="shimmer__img h-40  bg-slate-300 rounded-t-xl mb-4"></div>
+        {/* description */}
+        <div className="shimmer__line l1 bg-slate-300 w-44 h-5 rounded-lg m-2 "></div>
+        <div className="shimmer__line l1 bg-slate-300 w-36 h-5 rounded-lg m-2"></div>
         
+      </div>
+    );
+
+    i++;
+  }
+  // console.log(array)
+
+  return (
+    <div className="shimmer__container flex flex-wrap m-auto w-10/12 mt-16 px-4 ">
+      {array}
     </div>
-        )
-}
+  );
+};
 
-
-export default ShimmerBody
+export default ShimmerBody;
