b4e5cf54d336f0d772661d40113db08043fefe51
Abhay kumar <abhayrobotics1908@gmail.com>
2024-07-22 23:53:06 +0530

    new dist files

---
+++ 004 Food App/dist/Grocery.f83e2b35.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire11e5;(0,e.register)("loR3J",function(r,l){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var o=e("ayMG0"),t=()=>(0,o.jsx)("h1",{children:"Grocery page will lots of child component, Hence bundled separately "})});
+//# sourceMappingURL=Grocery.f83e2b35.js.map
